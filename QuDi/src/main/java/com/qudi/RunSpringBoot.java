ec7f3df99202e5edbb5ed264dee5ee00dd87a9d2
AlanAtlantis <liutao30101002@163.com>
2019-06-12 10:43:51 +0800

    background

---
+++ QuDi/src/main/java/com/qudi/RunSpringBoot.java
@@ -0,0 +1,18 @@
+package com.qudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RunSpringBoot  {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RunSpringBoot.class, args);
+	}
+
+	
+	
+
+}
---
+++ QuDi/src/main/java/com/qudi/dao/GoodsCategoryDao.java
@@ -0,0 +1,17 @@
+package com.qudi.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.mybatis.spring.annotation.MapperScan;
+
+import com.qudi.bean.GoodsCategory;
+
+@MapperScan
+public interface GoodsCategoryDao {
+
+	List<GoodsCategory> selectGoodsCategory();
+
+	List<GoodsCategory> selectGoodsCategoryLevel(@Param("id") int id, @Param("categoryLevel") int categoryLevel);
+
+}
